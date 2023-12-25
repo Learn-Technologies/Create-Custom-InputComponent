8690fc0a9a10e10e1f40357403748804023c7259
Learn-Technologies <151466377+Learn-Technologies@users.noreply.github.com>
2023-12-25 15:27:15 +0530

    first commit

---
+++ src/components/services/FirebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0j7PYIan_JbP0feLQFLOAGfyKtrL4doA",
+  authDomain: "yt-test-dd17b.firebaseapp.com",
+  projectId: "yt-test-dd17b",
+  storageBucket: "yt-test-dd17b.appspot.com",
+  messagingSenderId: "647121239973",
+  appId: "1:647121239973:web:f9b77f2442944fa0db724c",
+};
